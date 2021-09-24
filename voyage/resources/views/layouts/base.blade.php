e7d6a63d457275dd63b351e438c5080e96807fe0
TonyCDalvin <colas.tony47@gmail.com>
2021-09-24 16:27:18 +0200

    header

--- voyage/resources/views/layouts/base.blade.php
+++ voyage/resources/views/layouts/base.blade.php
@@ -62,7 +62,7 @@
                 <div class="row align-items-center ">
                     <div class="col-lg-12">
                         <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
-                            <a class="navbar-brand" href="index.html"> <img src="img/logo.png" alt="logo"> </a>
+                            <a class="navbar-brand" href="{{ route('home')}}"> <img src="img/logo.png" alt="logo"> </a>
                             <button class="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                 aria-expanded="false" aria-label="Toggle navigation">
@@ -79,30 +79,7 @@
                                         <a class="nav-link" href="{{ route('aboutus')}}">About</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="packages.html">packages</a>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown"
-                                            role="button" data-toggle="dropdown" aria-haspopup="true"
-                                            aria-expanded="false">
-                                            Blog
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="blog.html">Blog</a>
-                                            <a class="dropdown-item" href="single-blog.html">Single blog</a>
-                                        </div>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
-                                            role="button" data-toggle="dropdown" aria-haspopup="true"
-                                            aria-expanded="false">
-                                            pages
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
-                                            <a class="dropdown-item" href="{{ route('trips')}}">top place</a>
-                                            <a class="dropdown-item" href="{{ route('details')}}">tour details</a>
-                                            <a class="dropdown-item" href="elements.html">Elements</a>
-                                        </div>
+                                        <a class="nav-link" href="{{ route('trips')}}">Trips</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{ route('contact')}}">Contact</a>
